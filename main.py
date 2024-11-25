4306e37c09a9fc793c7713e8603e45be0d9835bb
Adarsha Bhattarai <42803692+adarsha30735@users.noreply.github.com>
2024-11-24 18:35:34 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+% echo "# Tumor-Segmentation-from-MRI-CT-Images" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/adarsha30735/Tumor-Segmentation-from-MRI-CT-Images.git
+git push -u origin main
